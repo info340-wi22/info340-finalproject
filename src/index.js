dab75d38823ededf22581353787208327174d672
Parsa ™ <parsak@uw.edu>
2022-02-28 16:57:19 -0800

    added normalize.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
