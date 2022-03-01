52c40865753d1d1d3a37fbb07a7114873e249598
Parsa ™ <parsak@uw.edu>
2022-02-28 17:32:59 -0800

    fixed citations/sources

--- src/Overview.js
+++ src/Overview.js
@@ -8,9 +8,9 @@ export function OverviewHeader() {
         <p className="h5">A tool to help hold yourself accountable and build healthy habits in and around your busy lifestyle.</p>
         <p className="lead">Complete your <a className="lead" href="check-in.html"> daily check-ins</a> to begin forming a
           visualization based off your habits.</p>
-        {/* <cite><a className="src"
-            href="https://pixabay.com/illustrations/triangles-shapes-the-geometry-of-the-3322776/"><br>Image
-            Source</br></a></cite> */}
+        <cite><a className="src"
+            href="https://pixabay.com/illustrations/triangles-shapes-the-geometry-of-the-3322776/">Image
+            Source</a></cite>
       </div>
 
     </div>
@@ -22,8 +22,8 @@ export function OverviewMain() {
     <main>
       <section>
         <img className="viz" src="./img/graph.png" alt="template visualization with empty charts." />
-        {/* <cite><a className="src" href="https://pixabay.com/vectors/gui-interface-internet-program-2311261/"><br>Image
-            Source</br></a></cite> */}
+        <cite><a className="src" href="https://pixabay.com/vectors/gui-interface-internet-program-2311261/">Image
+            Source</a></cite>
       </section>
       <div className="row d-flex justify-content-center align-content-center">
         <button className="btn btn-primary" href="index.html"> Daily Check-in </button>
@@ -31,9 +31,9 @@ export function OverviewMain() {
       <div className="sustainability-pledge">
         <img className="fit-picture" src="./img/heart.png" alt="minimalist leaf icon" />
         <p className="motto">WorkWell is a student led organization attributed to bettering human wellbeing.</p>
-        {/* <cite><a className="src"
-            href="https://www.flaticon.com/premium-icon/heart_5451167?term=leaf%20heart&page=1&position=4&page=1&position=4&related_id=5451167&origin=search"><br>Image
-            Source</br></a></cite> */}
+        <cite><a className="src"
+            href="https://www.flaticon.com/premium-icon/heart_5451167?term=leaf%20heart&page=1&position=4&page=1&position=4&related_id=5451167&origin=search">Image
+            Source</a></cite>
       </div>
     </main>
   );
