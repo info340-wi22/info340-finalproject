db67d7551445578319e693b6646d14f7efaecd6f
bryanb25 <bryanb25@uw.edu>
2022-03-14 04:33:08 -0700

    Added firebase implementation

--- src/Goals.js
+++ src/Goals.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { getDatabase, ref, set as firebaseSet, push as firebasePush, onValue } from 'firebase/database';
 // Our apps interactivity for the REACT draft can be found below
 export function GoalHeader() {
     return (
@@ -12,7 +14,7 @@ export function GoalHeader() {
     )
 }
 
-function GoalCard(props) {
+function DailyGoalCard(props) {
     const [status, setStatus] = useState(false);
 
     const handleYes = (event) => {
@@ -31,7 +33,7 @@ function GoalCard(props) {
                 <span className="material-icons-outlined">{props.goal.type}</span>
             </div>
             <div className="goal-content">
-                <h2 className="goal-title">{props.goal.title}</h2>
+                <h3 className="goal-title">{props.goal.title}</h3>
                 <p className="card-text">{props.goal.desc} </p>
                 <b>{status ? 'COMPLETED' : 'INCOMPLETE'}</b>
                 <div className="button">
@@ -43,16 +45,92 @@ function GoalCard(props) {
     )
 }
 
+function NewGoalCard(props) {
+    const [status, setStatus] = useState(false);
+
+    const handleYes = (event) => {
+        props.adoptCallback(props.goal.title);
+        setStatus(true);
+    }
+
+    const handleNo = (event) => {
+        setStatus(false);
+    }
+
+    return (
+        <div className={status ? "goal-complete":"goal"}>
+            <div className="goal-content">
+                <p className="card-text">{props.goal} </p>
+                <b>{status ? 'COMPLETED' : 'INCOMPLETE'}</b>
+                <div className="button">
+                    <button className="yesno-btn" type="button" onClick={handleYes}>YES</button>
+                    <button className="yesno-btn" type="button" onClick={handleNo}>NO</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export function GoalMain(props) {
-    console.log(props.goals);
     const goalArrayObj = props.goals.map((goalObject) => {
-        return <GoalCard key={goalObject.title} goal={goalObject} adoptCallback={props.adoptCallback} />
+        return <DailyGoalCard key={goalObject.title} goal={goalObject} adoptCallback={props.adoptCallback} />
+    })
+
+    const [userInput, setUserInput] = useState("");
+    const [goalArray, setGoalArray] = useState([]);
+
+    const db = getDatabase();
+
+    useEffect(() => {
+        const allDescRef = ref(db, "allDesc");
+        const offFunction = onValue(allDescRef, (snapshot) => {
+            const allDescObject = snapshot.val();
+            const descKeyArray = Object.keys(allDescObject);
+            const allDescArray = descKeyArray.map((keyString) => {
+                const whichObject= allDescObject[keyString];
+                return whichObject;
+            })
+            setGoalArray(allDescArray);
+        })
+        function cleanup() {
+            offFunction();
+        }
+        return cleanup;
+    }, [])
+
+    const handleSubmit = (event) => {
+        const allDescRef = ref(db, "allDesc");
+        firebasePush(allDescRef, userInput);
+        setUserInput("");
+    }
+
+    const handleChange = (event) => {
+        const inputValue = event.target.value;
+        setUserInput(inputValue);
+    }
+
+    const newGoalObj = goalArray.map((goalObject) => {
+        return <NewGoalCard key={goalObject} goal={goalObject} adoptCallback={props.adoptCallback} />
     })
 
     return (
         <main>
+            <h2>Daily Goals</h2>
             <div className='goal-container'>
                 {goalArrayObj}
+            </div>
+
+            <h2>Personal Goals</h2>
+            <div>
+                {newGoalObj}
+            </div>
+            <div className="form">
+                <div className="form-group mb-1">
+                    <input type="text" id="task-input" className="form-control" placeholder="Add personal goals?" onChange={handleChange} value={userInput}/>
+                </div>
+                <div className="form-group">
+                    <button type="button" id="add-task-button" className="btn btn-primary" onClick={handleSubmit}>Add goal to list</button>
+                </div>
             </div>
         </main>
     )
--- src/index.js
+++ src/index.js
@@ -8,6 +8,29 @@ import './index.css';
 
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDeGhtDDPdKqw99Rmbg-7kU4Zm-zB9vviE",
+  authDomain: "react-info340project-wi22.firebaseapp.com",
+  databaseURL: "https://react-info340project-wi22-default-rtdb.firebaseio.com",
+  projectId: "react-info340project-wi22",
+  storageBucket: "react-info340project-wi22.appspot.com",
+  messagingSenderId: "171952014356",
+  appId: "1:171952014356:web:8b06b6b9cf5033ffbb5301",
+  measurementId: "G-3G4NYXGNKF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const goalsArray = [
     { type: "fitness_center", title: "Push Up", desc: "Have you completed 100 push ups?" },
     { type: "water_drop", title: "Drink Water", desc: "Have you drank 60oz of water?" },
